3a36c3d1f047b052f782488c24b383ab47d86922
prsh2000 <thepratiksharma@gmail.com>
2023-10-18 17:20:19 +0530

    todo list app is created

---
+++ src/main/java/com/geekster/project/ToDo/Application/ToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.geekster.project.ToDo.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoAppApplication.class, args);
+	}
+
+}
